4c3fffe6ee6ff537013f3785d824cd4de2103192
Oleksii Ukhalov <143124367+Ratarekko@users.noreply.github.com>
2025-06-15 03:08:13 +0200

    Create basic CLI application structure

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"os"
+	"strings"
+
+	lab2 "github.com/LeVasTiaN/KPI_Lab2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputFile       = flag.String("f", "", "File with expression to compute")
+	outputFile      = flag.String("o", "", "Output file for result")
 )
 
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if (*inputExpression == "" && *inputFile == "") || (*inputExpression != "" && *inputFile != "") {
+		fmt.Fprintf(os.Stderr, "Error: Must provide exactly one of -e (expression) or -f (file)\n")
+		os.Exit(1)
+	}
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	fmt.Println("CLI app structure ready")
 }
